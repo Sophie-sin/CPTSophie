3edf90a091c74aef8e06a5e6cea5dc339cb57dca
Sophie-sin <chingyisophie.sin26@ycdsbk12.ca>
2025-05-23 09:33:01 -0400

    Update CPTSophie.java

--- CPTSophie.java
+++ CPTSophie.java
@@ -4,5 +4,7 @@ public class CPTSophie{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello world");
+		con.println("Konnichiwa");
+		con.println("nice to meet you");
 	}
 }
